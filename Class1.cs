a7a5e68337a1897b2ac63e04f2750b602e665bba
Redheart <37662575+RedheartWR@users.noreply.github.com>
2018-06-19 16:20:24 +0300

    Подготовка(1)

---
+++ Class1.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ClassLibrary1
+{
+    public class Data
+    {
+        private int[] V;
+
+        public double M
+        { get
+            {
+                try
+                {
+                    int sum = 0;
+                    for (int i = 0; i < V.Length; i++) sum += V[i];
+                    return sum / V.Length;
+                }
+                catch (DivideByZeroException) { return 0; }
+            }
+        }
+
+        public double D
+        {
+            get
+            {
+                try
+                {
+                    double sum = 0;
+                    for (int i = 0; i < V.Length; i++) sum += (V[i] - this.M) * (V[i] - this.M);
+                    return sum / V.Length;
+                }
+                catch(DivideByZeroException) { return 0; }
+            }
+        }
+
+        public Data(string str)
+        {
+            string[] s = str.Split(' ');
+            V = new int[s.Length - 1];
+            for (int i = 0; i < s.Length - 1; i++)
+            {
+                int.TryParse(s[i], out int a);
+                V[i] = a;
+            }
+        }
+
+        public override string ToString()
+        {
+            string str = "";
+            foreach (int a in V) str += " "+ a;
+            return str;
+        }
+    }
+
+    public class Processing : IEnumerable<Data>
+    {
+        private string _fileName;
+        private TextReader _textReader;
+
+        public Processing(string fileName)
+        {
+            _fileName = fileName;
+        }
+
+        public IEnumerator<Data> GetEnumerator()
+        {
+            return new InternalClass(this);
+        }
+
+        class InternalClass : IEnumerator<Data>
+        {
+            private Data _element;
+            private Processing _processing;
+
+            public InternalClass(Processing processing)
+            {
+                _processing = processing;
+                processing._textReader = new StreamReader(_processing._fileName);
+            }
+
+            public void Dispose()
+            {
+                _processing._textReader.Close();
+            }
+
+            public bool MoveNext()
+            {
+                string s = _processing._textReader.ReadLine();
+                if (s != null)
+                {
+                    _element = new Data(s);
+                    return true;
+                }
+                Reset();
+                return false;
+            }
+
+            public void Reset()
+            {
+                _processing._textReader = new StreamReader(_processing._fileName);
+            }
+
+            public Data Current => _element;
+
+            object IEnumerator.Current => Current;
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return new InternalClass(this);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using ClassLibrary1;
+
+namespace Подготовка
+{
+    class Program
+    {
+        public static void Create(string path, uint s, uint k, uint f)
+        {
+            StreamWriter file = new StreamWriter(new FileStream(path, FileMode.OpenOrCreate));
+            Random r = new Random();
+            uint n;
+            for (int i = 0; i < s; i++)
+            {
+                n = (uint)r.Next(1, (int)k);
+                string str = "";
+                for (int j = 0; j < n; j++)
+                {
+                    str += r.Next(1, (int)f) + " ";
+                }
+                file.WriteLine(str);
+            }
+
+            file.Close();
+        }
+
+        static void Main(string[] args)
+        {
+            Console.Write("s=");
+            uint.TryParse(Console.ReadLine(), out uint s);
+            Console.Write("k=");
+            uint.TryParse(Console.ReadLine(), out uint k);
+            Console.Write("f=");
+            uint.TryParse(Console.ReadLine(), out uint f);
+
+            string file_name = @"..\..\..\data.txt"; // подъем вверх на три папки
+            Create(file_name, s, k, f);
+
+            Processing p = new Processing(file_name);
+            foreach (Data a in p) Console.WriteLine(a.ToString());
+            var ord = from a in p orderby a.D/a.M descending select a;
+            foreach (Data a in ord) Console.WriteLine(a.D/a.M + '\t' + a.ToString());
+        }
+    }
+}
